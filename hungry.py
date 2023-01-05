0a620b2f1c6d4c8fdeca333a72a1a42510ad37ec
chandana <chandanajktym@gmail.com>
2023-01-05 13:20:19 +0530

    PY4: added another line

--- hungry.py
+++ hungry.py
@@ -3,5 +3,6 @@ n = int(input("Hello enter a number : "))
 if n%2 == 0:
     print("The number is even")
     print("The number is divisible by 2")
+    print("Hello ")
 else:
     print("The number is odd")
